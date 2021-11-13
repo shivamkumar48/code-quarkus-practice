825c970455db3c4b0dad5e00ddc0793fb181281b
Kumar <shikumar@luxoft.com>
2021-11-13 18:34:01 +0530

    quarkus grpc sample

--- src/main/java/com/shivam/HelloGrpcService.java
+++ src/main/java/com/shivam/HelloGrpcService.java
@@ -1,17 +1,20 @@
 package com.shivam;
 
-//import io.quarkus.grpc.GrpcService;
+import io.quarkus.grpc.GrpcService;
 
+import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 
-//@GrpcService
-public class HelloGrpcService /*implements HelloGrpc*/ {
+@GrpcService
+@Blocking
+public class HelloGrpcService implements HelloGrpc {
 
-    /*@Override
+    @Override
     public Uni<HelloReply> sayHello(HelloRequest request) {
+        System.out.println("Request received "+request.getName());
         return Uni.createFrom().item("Hello " + request.getName() + "!")
                 .map(msg -> HelloReply.newBuilder().setMessage(msg).build());
-    }*/
+    }
 
     public void greetHllo(){
         System.out.println("hello");
